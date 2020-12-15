fa33f189213de655d6b0fe42eace39e1f09bae06
Moez Bouhlel <lejenome@tik.tn>
2020-12-15 21:24:57 +0100

    upgrade deps

---
+++ docs/assets/js/embed.js
@@ -0,0 +1,46 @@
+"use strict";
+
+function initGist() {
+  const embedsC = document.getElementsByClassName("embed-gist");
+  for (const embedC of embedsC) {
+    if (embedC.classList.contains("embed-loaded")) {
+      continue;
+    }
+    const opt = embedC.dataset;
+    embedC.innerHTML = `
+    <script src="https://gist.github.com/${opt.gist}.js?"></script>
+    `;
+    embedC.classList.add("embed-loaded");
+  }
+}
+
+function initPlunker() {
+  const embedsC = document.getElementsByClassName("embed-plnkr");
+  for (const embedC of embedsC) {
+    if (embedC.classList.contains("embed-loaded")) {
+      continue;
+    }
+    const opt = embedC.dataset;
+    embedC.innerHTML = `
+      <iframe height='${opt.height || 265}' scrolling='no' src='https://embed.plnkr.co/gist/${opt.gist}?show=${opt.show || "preview"}' frameborder='no'  allowfullscreen='true' style='width: 100%;'></iframe>
+    `;
+    embedC.classList.add("embed-loaded");
+  }
+}
+
+function initCodePen() {
+  const embedsC = document.getElementsByClassName("embed-codepen");
+  for (const embedC of embedsC) {
+    if (embedC.classList.contains("embed-loaded")) {
+      continue;
+    }
+    const opt = embedC.dataset;
+    embedC.innerHTML = `
+      <iframe height='${opt.height || 265}' scrolling='no' title='${opt.title}' src='https://codepen.io/${opt.username}/embed/${opt.id}/?height=${opt.height || 265}&theme-id=dark&default-tab=${opt.tab || "result"}&embed-version=2' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'></iframe>
+    `;
+    embedC.classList.add("embed-loaded");
+  }
+}
+initGist();
+initPlunker();
+initCodePen();
---
+++ docs/cours/demos/balise-h1.html
@@ -0,0 +1,4 @@
+<h1>Mon titre principal</h1>
+<h2>Mon titre de section</h2>
+<h3>Mon sous-titre</h3>
+<h4>Mon sous-sous-titre</h4>
